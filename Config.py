12c7d4bee71dd2e23a4c377f748b0b71810a71ea
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2022-12-08 01:50:30 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOJQBuzO39VMFL5YE7npso-Q8zypjl5bcnXiQSCa478p3N_oUU0sLXyGwJFYihDOYINgljAtq0FeI6rJyydI0iuGeTApi0oKrM87driS5PW_EWEnEX4tlKfjQrrExawq90jUOFT0ojH5iKVG8Z2LlkiXTfQ0li4TsFF4ZxmbzFdDbsg5f0vxoT4NzVajAsjLMCmZE7HPaKcj0wDkhFcFKdmJ6XWVsFnK4XRG85vQLcfH9Gh7wPlz6RvLbSriSt3KC2DpSSXQcdbEet4n-Ro3F2D5hGzTAa1_aHtPTcl991KBuPBtVCffcCgqcU0sGwtHVE64telpyOeOij5r25agG3Itj81A=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Skyzo_Musicbot")
     SUPPORT = os.environ.get("SUPPORT", "Skyzo_musicbot") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "Skyzo_musicbot") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
